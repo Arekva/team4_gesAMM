264892de341e9434d82fcc6ab0049388628154e7
Lallapalooza <poubelle.g8@mail.com>
2020-11-13 14:17:16 +0100

    Push de fin de séance

---
+++ src/Mama/Mama/Medicament.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mama
+{
+    class Medicament
+    {
+        private string depotLegal;
+        private string nomCommercial;
+        private string composition;
+        private string effets;
+        private string contreIndication;
+        private string AMM;
+    }
+}
