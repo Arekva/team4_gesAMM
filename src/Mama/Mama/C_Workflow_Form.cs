1f09b9bc7a1ee0b365f2b79a7c3fce4ce6612338
Theo Lecoublet <53373927+YobiNoShio@users.noreply.github.com>
2020-12-04 14:27:30 +0100

    new forms

---
+++ src/Mama/Mama/C_Workflow_Form.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mama
+{
+    public partial class C_Workflow_Form : Form
+    {
+        public C_Workflow_Form()
+        {
+            InitializeComponent();
+        }
+    }
+}
