aa2dc2f25062f209f6e8c9653c5366980c1f27a1
Arthur <tutur716@gmail.com>
2020-12-04 17:18:04 +0100

    Ajout de la différence entre étape normée et non normée

--- src/Mama/Mama/Etape.cs
+++ src/Mama/Mama/Etape.cs
@@ -14,19 +14,16 @@ namespace Mama
         /// <param name="libelle">Le libellé de l'étape.</param>
         /// <param name="norme">Le libellé de la norme.</param>
         /// <param name="date">La date où la norme à été appliquée.</param>
-        public Etape(int num, string libelle, string norme, DateTime date)
+        public Etape(int num, string libelle)
         {
             this._Numero = num;
             this._Libelle = libelle;
-            this._Norme = norme;
-            this._Date = date;
         }
         
 
         private int _Numero = 0;
         private string _Libelle = null;
-        private string _Norme = null;
-        private DateTime _Date;
+        
 
 
         public int getNumero()
@@ -38,18 +35,5 @@ namespace Mama
         {
             return this._Libelle;
         }
-
-        public string getNorme()
-        {
-            return this._Norme;
-        }
-
-        public DateTime getDate()
-        {
-            return this._Date;
-        }
-        
-        
-        
     }
 }
