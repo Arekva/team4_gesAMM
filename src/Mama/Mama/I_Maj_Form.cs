df0679068b0f4d9003484ad60285a2478f734d22
Theo Lecoublet <53373927+YobiNoShio@users.noreply.github.com>
2020-12-04 14:21:00 +0100

    new forms

---
+++ src/Mama/Mama/I_Maj_Form.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mama
+{
+    public partial class I_Maj_Form : Form
+    {
+        public I_Maj_Form()
+        {
+            InitializeComponent();
+        }
+    }
+}
