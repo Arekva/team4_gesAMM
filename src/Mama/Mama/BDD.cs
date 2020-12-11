3306fdd712934dc5b922bd75d307a9cffb3880d9
Lallap <poubelle.g8@gmail.com>
2020-12-11 15:49:20 +0100

    finnalisation de la fonction de récupération de tous les subir

--- src/Mama/Mama/BDD.cs
+++ src/Mama/Mama/BDD.cs
@@ -196,17 +196,8 @@ namespace Mama
 
             while (reader.Read())
             {
-             
-
-                //lesSubirs.Add(new Subir(DateTime.Parse(reader["SUB_dateDecisions"].ToString()), Globale.Etapes[int.Parse(reader["SUB_numEtape"])]
-                    
-
-
-
-
-
-
-                    ));
+                lesSubirs.Add(new Subir(DateTime.Parse(reader["SUB_dateDecisions"].ToString()), Globale.Etapes[int.Parse(reader["SUB_numEtape"].ToString().TrimEnd())],
+                    int.Parse(reader["SUB_idDecision"].ToString().TrimEnd()), reader["SUB_codeDepotLegal"].ToString().TrimEnd()));                 
             }
             reader.Close();
 
